c36214bc250dfda695bfc06b9084585716ee9089
waldysetio <waldysetiono@gmail.com>
2022-02-19 21:48:58 +0700

    Update bot commands

--- bot.py
+++ bot.py
@@ -4,8 +4,12 @@ import telebot
 API_KEY = os.getenv("API_KEY")
 bot = telebot.TeleBot(API_KEY)
 
-@bot.message_handler(commands=["Greet"])
+@bot.message_handler(commands=["start"])
 def greet(message):
-    bot.reply_to(message, "Hello")
+    bot.reply_to(message, "Welcome to Earthquake Warning Bot")
+    
+@bot.message_handler(commands=["hello"])
+def hello(message):
+    bot.send_message(message.chat.id, "Hello")
 
 bot.polling()
