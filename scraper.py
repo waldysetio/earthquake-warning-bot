0fe2f155afa0dd124a033b588db05a339148645a
waldysetio <waldysetiono@gmail.com>
2022-02-19 23:13:39 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,17 +12,18 @@ def getinfo():
   #Parse the page
   if page.status_code == 200:    
     #Parse the page to variable soup
-    soup = BeautifulSoup(page.text, "xml")  
+    soup = BeautifulSoup(page.text, "html.parser")  
 
     #Create array of required informations
-    info = ["Tanggal", "Jam", "DateTime", "Lintang", "Bujur", "Magnitude", 
-            "Kedalaman", "Wilayah", "Potensi"]
+    info = ["tanggal", "jam", "datetime", "lintang", "bujur", "magnitude", 
+            "kedalaman", "wilayah", "potensi"]
 
     #Get the information from the parsed page
     print("Telah terjadi gempa dengan keterangan sebagai berikut: ")
     for word in info:
       data = soup.find(word)
-      print(word, ":", data.text)
+      data = data.text
+      print(word.capitalize(), ":", data.capitalize())
 
   else:
     print("The source can't be accessed.")
